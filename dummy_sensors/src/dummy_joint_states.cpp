7b409f222fada57d90a52a1894c50703bece20e3
Sander van Dijk <sgvandijk@gmail.com>
2018-11-17 16:24:31 +0000

    Add left leg

--- dummy_sensors/src/dummy_joint_states.cpp
+++ dummy_sensors/src/dummy_joint_states.cpp
@@ -28,6 +28,12 @@ int main(int argc, char * argv[])
   msg->name.push_back("r_shoulder_pitch_joint");
   msg->name.push_back("r_shoulder_roll_joint");
   msg->name.push_back("r_elbow_joint");
+  msg->name.push_back("l_hip_yaw_joint");
+  msg->name.push_back("l_hip_roll_joint");
+  msg->name.push_back("l_hip_pitch_joint");
+  msg->name.push_back("l_knee_joint");
+  msg->name.push_back("l_ankle_pitch_joint");
+  msg->name.push_back("l_ankle_roll_joint");
   for (size_t i = 0; i < msg->name.size(); ++i)
     msg->position.push_back(0.0);
 
