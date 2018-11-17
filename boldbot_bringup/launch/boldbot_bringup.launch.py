d97ba7bfc6908fb75c7003572fdc4edef4d4f87e
Sander van Dijk <sgvandijk@gmail.com>
2018-11-17 14:06:42 +0000

    Package to bring up the boldbot robot model

---
+++ boldbot_bringup/launch/boldbot_bringup.launch.py
@@ -0,0 +1,14 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    urdf = os.path.join(get_package_share_directory('boldbot_description'),
+                        'urdf', 'boldbot.urdf')
+    return LaunchDescription([
+        Node(package='robot_state_publisher', node_executable='robot_state_publisher',
+             output='screen', arguments=[urdf]),
+])
