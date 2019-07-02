5ea31e9e59bdf5f830f3bf2666fe9d3c37084af7
Marcus M. Scheunemann <find@mms.ai>
2019-07-02 19:06:13 +1000

    initial debug behavior (walking straight)

---
+++ behavior/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'behavior'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[],
+    zip_safe=False,
+    py_modules=[
+        'walk_straight'
+    ],
+    data_files=[],
+    install_requires=['setuptools'],
+    maintainer='Marcus M. Scheunemann',
+    maintainer_email='marcus@mms.ai',
+    keywords=['ROS'],
+    description='RC19 winning behavior',
+    license='Apache License, Version 2.0',
+    entry_points={
+        'console_scripts': [
+            'walk_straight = walk_straight:main'
+        ],
+    },
+)
---
+++ behavior/walk_straight.py
@@ -0,0 +1,41 @@
+# Copyright 2019 Bold Hearts
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+
+from geometry_msgs.msg import Twist
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = rclpy.create_node('walking_straight_publisher')
+    publisher = node.create_publisher(Twist, '/walking/command', 10)
+
+    msg = Twist(
+        linear=(0,0,0),
+        angular=(0,0,0)
+    )
+
+    def timer_callback():
+        node.get_logger().info('Publishing: {}'.format(msg))
+        publisher.publish(msg)
+
+    timer = node.create_timer(1, timer_callback)
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
