e47104fe74ce215c7a61cb789da4fd302ab9c473
Sander G. van Dijk <sgvandijk@gmail.com>
2019-12-24 15:29:54 +0100

    Control motors with PID controllers

--- boldbot_gazebo_plugin/include/boldbot_gazebo_plugin/boldbot_gazebo_plugin.hpp
+++ boldbot_gazebo_plugin/include/boldbot_gazebo_plugin/boldbot_gazebo_plugin.hpp
@@ -15,6 +15,7 @@
 #ifndef BOLDBOT_GAZEBO_PLUGIN__BOLDBOT_GAZEBO_PLUGIN_HPP_
 #define BOLDBOT_GAZEBO_PLUGIN__BOLDBOT_GAZEBO_PLUGIN_HPP_
 
+#include <gazebo/common/PID.hh>
 #include <gazebo/common/Plugin.hh>
 #include <rclcpp/rclcpp.hpp>
 #include <mx_joint_controller_msgs/msg/joint_command.hpp>
@@ -39,9 +40,12 @@ private:
   gazebo::physics::ModelPtr model_;
   gazebo::physics::WorldPtr world_;
 
+  gazebo::common::Time last_sim_time_;
+
   gazebo::event::ConnectionPtr update_connection_;
 
-  std::map<std::string, gazebo::physics::JointPtr> joints_;
+  std::map<std::string, std::pair<gazebo::physics::JointPtr, gazebo::common::PID>> joints_;
+  std::map<std::string, double> joint_targets_;
 
   rclcpp::Node::SharedPtr ros_node_;
   rclcpp::Subscription<JointCommand>::SharedPtr joint_command_sub_;
--- boldbot_gazebo_plugin/src/boldbot_gazebo_plugin.cpp
+++ boldbot_gazebo_plugin/src/boldbot_gazebo_plugin.cpp
@@ -7,7 +7,8 @@ namespace boldbot_gazebo_plugin
 {
 
 BoldbotGazeboPlugin::BoldbotGazeboPlugin()
-: robot_namespace_{""}
+: robot_namespace_{""},
+  last_sim_time_{0}
 {
 }
 
@@ -41,7 +42,14 @@ void BoldbotGazeboPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr 
       continue;
     }
 
-    joints_[j->GetName()] = j;
+    auto pid = gazebo::common::PID{};
+    pid.SetPGain(200.0);
+    pid.SetIGain(0.0);
+    pid.SetDGain(0.0);
+
+    auto const & name = j->GetName();
+    joints_[name] = std::make_pair(j, pid);
+    joint_targets_[name] = 0.0;
   }
 
   RCLCPP_DEBUG(ros_node_->get_logger(), "Got joints:");
@@ -56,6 +64,29 @@ void BoldbotGazeboPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr 
 
 void BoldbotGazeboPlugin::Update()
 {
+  auto cur_time = world_->SimTime();
+  if (last_sim_time_ == 0) {
+    last_sim_time_ = cur_time;
+    return;
+  }
+
+  auto dt = (cur_time - last_sim_time_).Double();
+  RCLCPP_DEBUG(ros_node_->get_logger(), std::to_string(dt));
+
+  // Update joint PIDs
+  for (auto & j : joints_) {
+    auto const & name = j.first;
+    auto & joint = j.second.first;
+    auto & pid = j.second.second;
+
+    auto error = joint->Position() - joint_targets_[j.first];
+
+    auto force = pid.Update(error, dt);
+    joint->SetForce(0, force);
+  }
+
+  last_sim_time_ = cur_time;
+
   rclcpp::spin_some(ros_node_);
 }
 
